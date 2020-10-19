60cfa9af3a36b7e6498360c2e9107b1291a9614b
unknown <lisa@fomolux.com.tw>
2020-10-19 09:27:51 +0800

    test

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express=require('express');
 const mongoose=require('mongoose');
-const cors=require('cors');
+// const cors=require('cors');
+const path=require('path');
 
 const app=express();
 mongoose.connect('mongodb+srv://lisa:117599@clustermern.k6f8p.mongodb.net/房卡管理?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-app.use(cors())
+// app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('app')
-})
+// app.get('/client', (req, res)=>{
+//     // res.send('app')
+// })
 
-app.use('/cards', require('./routes/cards') )
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+
+// app.use('/cards', require('./routes/cards') )
+app.get('/', (req, res)=>{
+    Card.find()
+    .then(data=>res.json(data))
+    .catch(err=>console.log(err))
+})
 
 const PORT=process.env.PORT || 9001;
 app.listen(PORT, ()=>{
